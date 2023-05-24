bf7dd5c0919698d5680f2b71cfdbc853f0086116
Lucio Berté <lucioaberte@gmail.com>
2023-05-24 17:44:04 -0300

    resume for deploy

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,7 +12,7 @@ import { SectionHeading, SectionTitle } from "../styles/SectionHeading";
 import { StyledParagraph } from "../styles/Typography.styled";
 import myImage from "../../assets/my-image.jpg";
 import downloadIcon from "../../assets/icons/download-icon.svg";
-import resume from "Resume.pdf";
+import resume from "/resume.pdf";
 
 const About = () => {
   return (
